53d8d5588f641bae2f118548c7cb276a9d4f2c04
Nivedita Naik <116743692+Nivedita-Naik@users.noreply.github.com>
2024-09-25 17:48:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "ColorPair.h"
- 
+  
 // Function prototypes for test functions
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
